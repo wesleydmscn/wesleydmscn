7d838e972f7338b380654b74957efed016cc8545
wesleydamasceno <wesleydamasceno.co@gmail.com>
2022-12-23 20:59:28 -0300

    refactor: remove hooks, update design and add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,35 @@
-import { useEffect, useState } from 'react';
+import { Analytics } from './components/Analytics';
 import { Links } from './components/Links';
+import { Navigation } from './components/Navigation';
+
 import './styles/global.css';
 
 export const App = () => {
-  const [avatar, setAvatar] = useState('');
-  const getGitHubProfile = async () => {
-    const response = await fetch('https://api.github.com/users/wesleydmscn');
-    const data = await response.json();
-
-    setAvatar(data.avatar_url);
-  }
-
-  useEffect(() => { getGitHubProfile() }, []);
-
   return (
-    <div className='sm:text-sm md:text-base w-screen h-screen bg-blue-dark grid place-items-center'>
-      <main className='m-8 grid gap-y-6 text-white'>
-        <header className='flex flex-wrap gap-6'>
-          <img 
-            className='w-16 h-16 border-2 border-blue rounded-full' 
-            src={avatar || './public/avatar.png'}
-            alt="Profile Avatar"
-          />
+    <div className='flex flex-wrap gap-16 m-8'>
+      <Navigation />
 
-          <div>
-            <h1 className='text-2xl text-blue font-bold'>Hello World.</h1>
-            <h2 className='text-2xl text-blue font-bold'>My name is Wesley Damasceno.</h2>
-          </div>
-        </header>
-
-        <p className='max-w-xl'>
-          I'm a software developer based in Brazil. Currently with knowledge in front-end and back-end with
-          JavaScript. Looking for an opportunity to use my coding <i className='text-blue font-bold'>skills</i>, solve complex problems and help complete
-          projects. Besides programming, I like video games, old movies, chess and math.
+      <main className='grid gap-y-6 text-white'>
+        <h1 className='text-3xl text-white font-normal'>Wesley Damasceno</h1>
+        <p className='max-w-xl text-gray'>
+          Computing enthusiast with ~1 year of experience looking for a career transition.
         </p>
 
-        <footer className='flex gap-x-6'>
-          <Links />
-        </footer>
+        <section className='flex gap-6'>
+          <img 
+            className='w-24 h-24 rounded-full grayscale hover:grayscale-0' 
+            src='/avatar.png' 
+            alt="Profile Avatar" 
+          />
+
+          <Analytics />
+        </section>
+
+        <p className='max-w-xl text-gray'>
+          Currently with knowledge in front-end and back-end with JavaScript. Looking for an opportunity to use my coding skills, solve complex problems and help complete projects. Besides programming, I like video games, old movies, chess and math.
+        </p>
+
+        <Links />
       </main>
     </div>
   );
