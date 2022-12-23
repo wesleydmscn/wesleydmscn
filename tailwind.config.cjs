7100db723f8c83312583536335315fdc95e4c0ee
wesleydamasceno <wesleydamasceno.co@gmail.com>
2022-12-23 20:54:44 -0300

    fix: colors in theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,7 +12,9 @@ module.exports = {
       },
       colors: {
         'blue': '#317BC0',
-        'blue-dark': '#0D1117',
+        'dark': '#111010',
+        'dark-gray': '#262626',
+        'gray': '#A3A3A3',
         'white': '#FFF',
       },
       fontFamily: {
