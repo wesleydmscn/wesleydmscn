6c7f1f41432e1b7b320ed000aada2a4f96abb378
wesleydamasceno <wesleydamasceno.co@gmail.com>
2022-12-23 21:03:34 -0300

    fix: update Analytics counters

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -3,17 +3,17 @@ export function Analytics() {
     <div className='grid gap-y-4'>
       <div className='flex gap-x-4 text-gray'>
         <img src="/twitter-icon.svg" alt="Twitter Logo" />
-        <p>35,050 tweets posted.</p>
+        <p>0 tweets posted.</p>
       </div>
 
       <div className='flex gap-x-4 text-gray'>
         <img src="/github-icon.svg" alt="GitHub Logo" />
-        <p>35,050 commits in the last year.</p>
+        <p>0 commits in the last year.</p>
       </div>
 
       <div className='flex gap-x-4 text-gray'>
         <img src="/trendup.svg" alt="Icon Trend Up" />
-        <p>35,050 views on the blog.</p>
+        <p>0 views on the blog.</p>
       </div>
     </div>
   )
